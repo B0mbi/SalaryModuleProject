bda0842efa27ab16f166b131b7edfad425a4fd15
Paweł Tatys <pawel.tatys@cfi.pl>
2021-04-11 12:26:22 +0200

    Basic Transaction Model

---
+++ SalaryModuleProject/SalaryModuleProject/Enums/TransactionEnums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalaryModuleProject.Enums
+{
+    public class TransactionEnums
+    {
+        public enum eTransactionType
+        {
+            Deposit = 0,
+            Withdraw = 1
+        }
+    }
+}
---
+++ SalaryModuleProject/SalaryModuleProject/Models/TransactionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static SalaryModuleProject.Enums.TransactionEnums;
+
+namespace SalaryModuleProject.Models
+{
+    public class TransactionModel
+    {
+        public int TransactionId { get; set; }
+        public string TransactionName { get; set; }
+        public decimal Amount { get; set; }
+        public eTransactionType TransactionType { get; set; }
+    }
+}
